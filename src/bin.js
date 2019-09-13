ba4475022257237cbb24bc8dc96b2b84dd8825f9
Tyler Stewart <tylers490@gmail.com>
2019-09-12 21:36:26 -0600

    feat: prompt for credentials
    Allow headless puppeteer by prompting for credentials and entering
    them with puppeteer instead.

--- src/bin.js
+++ src/bin.js
@@ -6,6 +6,7 @@ const puppeteer = require('puppeteer');
 
 const {PLATFORM_CODES, PLATFORM_NAMES, GAMES} = require('./const');
 
+const {getChromeExecutablePath} = require('./chrome');
 const {timeout} = require('./helpers');
 const {fetchShiftKeys} = require('./codes');
 const {keyCacheFactory} = require('./cache');
@@ -17,81 +18,113 @@ const {waitForShiftLogin, getProfileEmail, redeemShiftKey} = require('./redeem')
     interactive: true
   });
 
-  const {platformIndex} = await prompt({
-    type: 'select',
-    name: 'platformIndex',
-    message: 'Platform',
-    choices: [
-      { title: 'PC', value: 0 },
-      { title: 'Playstation', value: 1 },
-      { title: 'Xbox', value: 2 },
-    ],
-  });
-  if (platformIndex === undefined) return;
+  statusLog.await('Launching...');
 
-  const platformCode = PLATFORM_CODES[platformIndex];
-  const platformName = PLATFORM_NAMES[platformIndex];
+  // Workaround for pkg
+  const executablePath = getChromeExecutablePath(puppeteer.executablePath());
 
-  const {gameIndex} = await prompt({
-    type: 'select',
-    name: 'gameIndex',
-    message: 'Game',
-    choices: [
-      { title: 'Borderlands GOTY', value: 0 },
-      { title: 'Borderlands 2', value: 1 },
-      { title: 'Borderlands: The Pre-Sequel', value: 2 }
-    ],
-  });
-  if (gameIndex === undefined) return;
-
-  const game = GAMES[gameIndex];
-
-  statusLog.await('Launching browser...');
   const browser = await puppeteer.launch({
-    headless: false
+    executablePath,
+    headless: true
   });
 
   try {
-    statusLog.await('Waiting for login...');
-    await waitForShiftLogin(browser);
-
-    statusLog.await('Getting profile email...');
-    const user = await getProfileEmail(browser);
-
-    const {getKeyCache, addKeyCache} = keyCacheFactory(user, platformCode, game);
-
-    statusLog.await('Fetching keys...');
-    const fetchedKeys = await fetchShiftKeys(browser, platformCode, game);
-
-    statusLog.await('Loading key cache...');
-    const usedKeys = getKeyCache();
-
-    statusLog.await('Redeeming keys...');
-
-    for (let key of fetchedKeys) {
-      const keyLog = new Signale({
-        scope: key,
-        interactive: true
-      });
-      keyLog.await('Redeeming key...');
-
-      if (!usedKeys.includes(key)) {
-        const [redeemed, message] = await redeemShiftKey(browser, platformName, key);
-        if (!redeemed) {
-          keyLog.error(message);
-        } else {
-          keyLog.success(message);
-        }
-
-        addKeyCache(key);
-
-        await timeout(1000);
-      } else {
-        keyLog.note('Key found in cache, skipped')
+    const {email, password} = await prompt([
+      {
+        type: 'text',
+        name: 'email',
+        message: 'Shift Email'
+      },
+      {
+        type: 'password',
+        name: 'password',
+        message: 'Shift Password'
       }
-    }
+    ]);
+    if (email === undefined) return;
+    if (password === undefined) return;
 
-    statusLog.complete('Complete!');
+    await waitForShiftLogin(browser, email, password);
+
+    let redeem = true;
+    while (redeem) {
+      const {platformIndex} = await prompt({
+        type: 'select',
+        name: 'platformIndex',
+        message: 'Platform',
+        choices: [
+          { title: 'PC', value: 0 },
+          { title: 'Playstation', value: 1 },
+          { title: 'Xbox', value: 2 },
+        ],
+      });
+      if (platformIndex === undefined) return;
+
+      const platformCode = PLATFORM_CODES[platformIndex];
+      const platformName = PLATFORM_NAMES[platformIndex];
+
+      const {gameIndex} = await prompt({
+        type: 'select',
+        name: 'gameIndex',
+        message: 'Game',
+        choices: [
+          { title: 'Borderlands GOTY', value: 0 },
+          { title: 'Borderlands 2', value: 1 },
+          { title: 'Borderlands: The Pre-Sequel', value: 2 }
+        ],
+      });
+      if (gameIndex === undefined) return;
+
+      const game = GAMES[gameIndex];
+
+      statusLog.await('Preparing...');
+      const user = await getProfileEmail(browser);
+
+      const {getKeyCache, addKeyCache} = keyCacheFactory(user, platformCode, game);
+
+      statusLog.await('Fetching keys...');
+      const fetchedKeys = await fetchShiftKeys(browser, platformCode, game);
+
+      statusLog.await('Loading key cache...');
+      const usedKeys = getKeyCache();
+
+      statusLog.await('Redeeming keys...');
+
+      for (let key of fetchedKeys) {
+        const keyLog = new Signale({
+          scope: key,
+          interactive: true
+        });
+        keyLog.await('Redeeming key...');
+
+        if (!usedKeys.includes(key)) {
+          const [redeemed, message] = await redeemShiftKey(browser, platformName, key);
+          if (!redeemed) {
+            keyLog.error(message);
+          } else {
+            keyLog.success(message);
+          }
+
+          addKeyCache(key);
+
+          await timeout(1000);
+        } else {
+          keyLog.note('Key found in cache, skipped')
+        }
+      }
+
+      statusLog.complete('Redeemed all keys!');
+
+      const {cont} = await prompt({
+        type: 'toggle',
+        name: 'cont',
+        message: '\nRedeem again on another platform?',
+        initial: true,
+        active: 'yes',
+        inactive: 'no'
+      });
+      redeem = cont;
+    }
   } catch (err) {
     statusLog.error(err);
   } finally {
