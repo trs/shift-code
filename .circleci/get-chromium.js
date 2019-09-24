453aeb18f465a5cdac9fb70a75869638a9aa0893
Tyler Stewart <tylers490@gmail.com>
2019-09-24 01:13:19 -0600

    chore: update packages

--- .circleci/get-chromium.js
+++ .circleci/get-chromium.js
@@ -1,14 +1,16 @@
 const puppeteer = require('puppeteer');
 const revision = require('puppeteer/package').puppeteer.chromium_revision;
 
+function writeDownloadProgress(downloadBytes, totalBytes) {
+  const percent = ((downloadBytes / totalBytes) * 100).toFixed(0);
+  process.stdout.write(`${percent}% (${downloadBytes} / ${totalBytes})\r`);
+}
+
 async function downloadChromium(platform) {
   console.log(`Downloading Chromium ${revision} (${platform})`);
   const browserFetcher = puppeteer.createBrowserFetcher({ platform });
-  await browserFetcher.download(revision, (downloadBytes, totalBytes) => {
-    const percent = ((downloadBytes / totalBytes) * 100).toFixed(0);
-    process.stdout.write(`${percent}% (${downloadBytes} / ${totalBytes})\r`);
-  });
-  await browserFetcher.localRevisions(); // TODO: Needed?
+  await browserFetcher.download(revision, writeDownloadProgress);
+  await browserFetcher.localRevisions();
 }
 
 (async function () {
