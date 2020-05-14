ad5679807b471ccfed11d44fecb87ffb935ee01a
Tyler Stewart <hello@tylerstewart.ca>
2020-05-13 21:52:09 -0600

    feat: test new workflow

--- .github/workflows/currentVersion.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-const path = require('path');
-const dir = process.argv[2];
-const package = require(path.resolve(process.cwd(), dir, 'package.json'));
-process.stdout.write(package.version);
