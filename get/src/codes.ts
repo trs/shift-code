f0e42110672a16ab9f10433a7923f4bf0c1e74e4
Tyler Stewart <git@tylerstewart.ca>
2025-10-05 22:00:51 -0600

    feat: remove fextralife code get

--- get/src/codes.ts
+++ get/src/codes.ts
@@ -1,9 +1,7 @@
 import { getOrcicornShiftCodes } from './source/orcicorn';
-import { getFextralifeShiftCodes } from './source/fextralife';
 import { getCodeVaultShiftCodes } from './source/codevault';
 
 export async function * getShiftCodes() {
-  yield * getFextralifeShiftCodes();
-  yield * getOrcicornShiftCodes();
   yield * getCodeVaultShiftCodes();
+  yield * getOrcicornShiftCodes();
 }
