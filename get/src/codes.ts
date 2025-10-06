c3ae2f889c65c55941820b2b8e774c00263a77e5
Marcelo Alves <selfmadecelo@gmail.com>
2025-10-05 20:25:40 -0700

    feat: add code vault api

--- get/src/codes.ts
+++ get/src/codes.ts
@@ -1,7 +1,9 @@
 import { getOrcicornShiftCodes } from './source/orcicorn';
 import { getFextralifeShiftCodes } from './source/fextralife';
+import { getCodeVaultShiftCodes } from './source/codevault';
 
 export async function * getShiftCodes() {
   yield * getFextralifeShiftCodes();
   yield * getOrcicornShiftCodes();
+  yield * getCodeVaultShiftCodes();
 }
